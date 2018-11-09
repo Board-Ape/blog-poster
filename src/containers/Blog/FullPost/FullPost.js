11d309fb7ec6f481311f0f33708c80a3d72c6958
Sam Singer <samuel.singer1@gmail.com>
2018-11-09 14:55:20 -0800

    Not showing post because we need to retrieve param

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
     }
 
     componentDidUpdate () {
-        if (this.props.id) {
+        if ( this.props.id ) {
             if (!this.state.loadedPost || this.state.loadedPost && this.state.loadedPost.id !== this.props.id) {
 
                 axios.get('/posts/' + this.props.id)
